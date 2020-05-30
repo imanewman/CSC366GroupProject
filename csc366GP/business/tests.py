b82587c88530c9a42e7c9ea932107b546c6ed4b6
Nicole Hill <32960665+nmhill@users.noreply.github.com>
2020-05-30 15:07:50 -0700

    Initial Commit w/ the skeletaon app and the coed to create the tables/database

---
+++ csc366GP/business/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from business.models import *
+# Create your tests here.
+
+class CustomerTestCase(TestCase):
+   def test_example(self):
+      #nothing is tested lol
+      nothing = "this does nothing"
